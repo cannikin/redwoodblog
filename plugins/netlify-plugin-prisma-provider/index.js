f6cea80e87bb0cee12d556111bccd88a48952c2c
Rob Cameron <rob.cameron@fastmail.com>
2020-04-02 11:51:28 -0700

    Refactor plugin

--- plugins/netlify-plugin-prisma-provider/index.js
+++ plugins/netlify-plugin-prisma-provider/index.js
@@ -2,17 +2,18 @@ const fs = require('fs')
 const path = require('path')
 const PRISMA_SCHEMA_PATH = path.join('api', 'prisma', 'schema.prisma')
 const PROVIDER_REGEX = /^\s+provider\s+=\s+["']sqlite["']/m
+const NEW_PROVIDER_STRING = `provider = "${process.env.DATABASE_PROVIDER ||
+  'postgresql'}"`
 
 module.exports = {
   onPreBuild: () => {
     const schema = fs.readFileSync(PRISMA_SCHEMA_PATH).toString()
 
     if (schema.match(PROVIDER_REGEX)) {
-      const newSchemaString = `provider = "${process.env.DATABASE_PROVIDER ||
-        'postgresql'}"`
-      const newSchema = schema.replace(PROVIDER_REGEX, newSchemaString)
+      const newSchema = schema.replace(PROVIDER_REGEX, NEW_PROVIDER_STRING)
+
       fs.writeFileSync(PRISMA_SCHEMA_PATH, newSchema)
-      console.log(`Replaced provider with \`${newSchemaString}\``)
+      console.log(`Replaced provider with \`${NEW_PROVIDER_STRING}\``)
     }
   },
 }
